be7558ea479afe0672f4a01bef647e83d22daecc
nayemalways <nishanahmed13913@gmail.com>
2025-12-06 11:57:15 +0600

    fixed indentation for registeration api

--- src/app/modules/users/user.routes.ts
+++ src/app/modules/users/user.routes.ts
@@ -7,12 +7,7 @@ import { Role } from './user.interface';
 
 const router = express.Router();
 
-router.post(
-  '/registration',
-  validateRequest(userZodSchema),
-  userControllers.registerUser
-);
-
+router.post('/registration', validateRequest(userZodSchema), userControllers.registerUser);
 router.get('/get_me', checkAuth(...Object.keys(Role)), userControllers.getMe);
 router.patch('/:userId', checkAuth(...Object.keys(Role)), userControllers.userUpdate);
 router.delete('/:userId', checkAuth(...Object.keys(Role)), userControllers.userDelete);
