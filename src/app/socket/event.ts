3f3bd3e2e93d7e24ee4d87fa0f0975aecc82c3f4
nayemalways <nishanahmed13913@gmail.com>
2025-12-11 11:43:42 +0600

    create socket event 'get_online_user'

--- src/app/socket/event.ts
+++ src/app/socket/event.ts
@@ -1,14 +1,10 @@
-/* eslint-disable no-console */
+
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
+
+
+
 export const SocketEvents = (socket: any) => {
-
-    // User joins their personal room
-    socket.on("join-user", (userId: string) => {
-        socket.join(userId);
-        console.log("User joined in the room: ", userId);
-    })
-
     // User joins event room
     socket.on("join-event", (eventId: string) => {
         socket.join(`event:${eventId.trim()}`);
--- src/app/socket/index.ts
+++ src/app/socket/index.ts
@@ -1,26 +1,41 @@
+/* eslint-disable @typescript-eslint/no-dynamic-delete */
 /* eslint-disable no-console */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Server } from "socket.io";
-import { SocketEvents } from "./event";
 
 export let io: Server;
+export const onlineUsers: Record<string, string> = {}; // userId -> socketId
 
+
+
+// Socket - Init
 export const initSocket = (server: any) => {
-    io = new Server(server, {
-        cors: {
-            origin: "*",
-            methods: ["GET", "POST"]
-        }
+  io = new Server(server, {
+    cors: { origin: '*', methods: ['GET', 'POST'] },
+  });
+
+  io.on('connection', (socket) => {
+    console.log('User connected:', socket.id);
+
+    let userId: string | null = null;
+
+    // Event: join-user
+    socket.on('join-user', (_userId: string) => {
+      userId = _userId;
+      socket.join(userId);
+      onlineUsers[userId] = socket.id;
+
+      console.log('User joined room:', userId);
+      io.emit('get_online_users', Object.keys(onlineUsers));
     });
 
-    io.on("connection", (socket) => {
-        console.log("User connected:", socket.id);
 
-        // All events Registered
-        SocketEvents(socket);
+    // Handle Disconnect
+    socket.on('disconnect', () => {
+      if (userId) delete onlineUsers[userId];
 
-        socket.on("disconnect", () => {
-            console.log("User disconnected", socket.id);
-        })
-    })
-}
+      io.emit('get_online_users', Object.keys(onlineUsers));
+      console.log('User disconnected:', socket.id);
+    });
+  });
+};
