82250d24e28787462ae958b1d5c970d9fade99d4
nayemalways <nishanahmed13913@gmail.com>
2025-12-12 15:59:48 +0600

    updated group model and interface

--- src/app/modules/groups/group.interface.ts
+++ src/app/modules/groups/group.interface.ts
@@ -2,10 +2,10 @@ import { Types } from 'mongoose';
 
 export interface IGroup {
   id?: Types.ObjectId;
+  event: Types.ObjectId;
   group_admin: Types.ObjectId;
   group_name: string;
   group_image: string;
   group_members: Types.ObjectId[];
   group_description: string;
-  media: string[]; // images
 }
