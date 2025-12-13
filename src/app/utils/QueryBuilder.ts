bcc14bb28fbeb04863dc8b3b15ae5d2914c3d98d
nayemalways <nishanahmed13913@gmail.com>
2025-12-13 17:19:55 +0600

    modified query builder

--- src/app/utils/QueryBuilder.ts
+++ src/app/utils/QueryBuilder.ts
@@ -1,90 +1,100 @@
-// /* eslint-disable @typescript-eslint/no-dynamic-delete */
-// import { Query } from 'mongoose';
-// import { excludeField } from '../modules/tour/tour.constant';
+/* eslint-disable @typescript-eslint/no-dynamic-delete */
+import { Query } from 'mongoose';
+import { excludeField } from '../modules/events/event.constant';
 
-// export class QueryBuilder<T> {
-//   public queryModel: Query<T[], T>;
-//   public query: Record<string, string>;
+export class QueryBuilder<T> {
+  public queryModel: Query<T[], T>;
+  public query: Record<string, string>;
 
-//   constructor(queryModel: Query<T[], T>, query: Record<string, string>) {
-//     this.queryModel = queryModel;
-//     this.query = query;
-//   }
+  constructor(queryModel: Query<T[], T>, query: Record<string, string>) {
+    this.queryModel = queryModel;
+    this.query = query;
+  }
 
-//   // Case Sensitive filtering
-//   filter(): this {
-//     const filter = { ...this.query };
-//     for (const value of excludeField) {
-//       delete filter[value];
-//     }
+  // Case Sensitive filtering
+  filter(): this {
+    const filter = { ...this.query };
+    for (const value of excludeField) {
+      delete filter[value];
+    }
 
-//     this.queryModel = this.queryModel.find(filter);
-//     return this;
-//   }
+    this.queryModel = this.queryModel.find(filter);
+    return this;
+  }
 
-//   // Searching
-//   search(searchableField: string[]): this {
-//     const searchTerm = this.query.searchTerm || '';
-//     const searchQuery = {
-//       $or: searchableField.map((field) => ({
-//         [field]: { $regex: searchTerm, $options: 'i' },
-//       })),
-//     };
+  // Text Based Searching on - title, description, venue
+  textSearch(): this {
+    const searchTerm = this.query.searchTerm;
+    if (!searchTerm) return this;
 
-//     this.queryModel = this.queryModel.find(searchQuery);
-//     return this;
-//   }
+    this.queryModel = this.queryModel
+      .find(
+        { $text: { $search: searchTerm } },
+        { score: { $meta: 'textScore' } } // relevance score
+      )
+      .sort({ score: { $meta: 'textScore' } });
 
-//   // Sorting
-//   sort(): this {
-//     const sort = this.query.sort || '-createdAt'; // ex: title, or -title
-//     this.queryModel = this.queryModel.sort(sort);
-//     return this;
-//   }
+    return this;
+  }
 
-//   // Field filtering
-//   select(): this {
-//     const fields = this.query.fields?.split(',').join(' ') || ''; // ex: "title description price"
-//     this.queryModel = this.queryModel.select(fields);
-//     return this;
-//   }
+  // Sorting
+  sort(): this {
+    const sort = this.query.sort || '-createdAt'; // ex: title, or -title
+    this.queryModel = this.queryModel.sort(sort);
+    return this;
+  }
 
-//   // Pagination
-//   paginate(): this {
-//     const page = Number(this.query.page) || 1;
-//     const limit = Number(this.query.limit) || 10;
-//     const skip = (page - 1) * limit;
+  // Field filtering
+  select(): this {
+    const fields = this.query.fields?.split(',').join(' ') || ''; // ex: "title description price" or "title,description,price"
+    this.queryModel = this.queryModel.select(fields);
+    return this;
+  }
 
-//     this.queryModel = this.queryModel.skip(skip).limit(limit);
-//     return this;
-//   }
+  // Pagination
+  paginate(): this {
+    const page = Number(this.query.page) || 1;
+    const limit = Number(this.query.limit) || 10;
+    const skip = (page - 1) * limit;
 
-//   join(refs: string[]): this {
-//     refs.forEach((ref) => {
-//       return (this.queryModel = this.queryModel.populate({ path: ref }));
-//     });
-//     // this.queryModel = this.queryModel.populate({ path: 'division' });
-//     // this.queryModel = this.queryModel.populate({ path: 'tourType' });
-//     return this;
-//   }
+    this.queryModel = this.queryModel.skip(skip).limit(limit);
+    return this;
+  }
 
-//   // Final build instance
-//   build() {
-//     return this.queryModel;
-//   }
+  // Join Collection Dynamically
+  join(): this {
+    const joinQuery = this.query?.join;
 
-//   // Generate meta data
-//   async getMeta() {
-//     const page = Number(this.query.page) || 1;
-//     const limit = Number(this.query.limit) || 10;
-//     const totalDocuments = await this.queryModel.model.countDocuments();
-//     const totalPage = Math.ceil(totalDocuments / limit);
+    if (!joinQuery) {
+      return this;
+    }
 
-//     return {
-//       page,
-//       limit,
-//       total: totalDocuments,
-//       totalPage,
-//     };
-//   }
-// }
+    const refs = joinQuery.split(',');
+
+    refs.forEach((ref) => {
+      return (this.queryModel = this.queryModel.populate({ path: ref }));
+    });
+
+    return this;
+  }
+
+  // Final build instance
+  build() {
+    return this.queryModel;
+  }
+
+  // Generate meta data
+  async getMeta() {
+    const page = Number(this.query.page) || 1;
+    const limit = Number(this.query.limit) || 10;
+    const totalDocuments = await this.queryModel.model.countDocuments();
+    const totalPage = Math.ceil(totalDocuments / limit);
+
+    return {
+      page,
+      limit,
+      total: totalDocuments,
+      totalPage,
+    };
+  }
+}
