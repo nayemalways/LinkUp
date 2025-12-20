b2be8e922a67708d36528376119226188a93f539
nayemalways <nishanahmed13913@gmail.com>
2025-12-20 16:49:34 +0600

    handled gruop photo upload and group image message uploading

--- src/app/modules/groups/group.route.ts
+++ src/app/modules/groups/group.route.ts
@@ -2,14 +2,18 @@ import express from 'express';
 import { checkAuth } from '../../middlewares/auth.middleware';
 import { groupControllers } from './group.controller';
 import { Role } from '../users/user.interface';
+import { groupZodSchema } from './group.validate';
+import { multerUpload } from '../../config/multer.config';
+import { validateRequest } from '../../middlewares/validateRequest';
+import { sendMessageSchema } from '../message/message.validate';
 
 const router = express.Router();
 
 // CREATE GROUP - Verified users only
-router.post('/create', checkAuth(...Object.keys(Role)), groupControllers.createGroup);
+router.post('/create', checkAuth(...Object.keys(Role)), multerUpload.single('file'), validateRequest(groupZodSchema), groupControllers.createGroup);
 
 // GET USER'S GROUPS
-router.get('/', checkAuth(), groupControllers.getUserGroups);
+router.get('/', checkAuth(...Object.keys(Role)), groupControllers.getUserGroups);
 
 // GET SINGLE GROUP
 router.get('/:groupId', checkAuth(), groupControllers.getSingleGroup);
@@ -17,35 +21,37 @@ router.get('/:groupId', checkAuth(), groupControllers.getSingleGroup);
 // INVITE USERS TO GROUP - Admin/Superadmin only
 router.post(
   '/:groupId/invite',
-  checkAuth(),
+   checkAuth(...Object.keys(Role)),
   groupControllers.addUsersToGroup
 );
 
 // SEND MESSAGE IN GROUP
 router.post(
   '/:groupId/message',
-  checkAuth(),
+  checkAuth(...Object.keys(Role)),
+  multerUpload.single('file'),
+  validateRequest(sendMessageSchema),
   groupControllers.sendGroupMessage
 );
 
 // GET GROUP MESSAGES
 router.get(
   '/:groupId/messages',
-  checkAuth(),
+   checkAuth(...Object.keys(Role)),
   groupControllers.getGroupMessages
 );
 
 // LEAVE GROUP
-router.delete('/:groupId/leave', checkAuth(), groupControllers.leaveGroup);
+router.delete('/:groupId/leave',  checkAuth(...Object.keys(Role)), groupControllers.leaveGroup);
 
 // REMOVE MEMBER FROM GROUP - Admin/Superadmin only
 router.delete(
   '/:groupId/member/:memberId',
-  checkAuth(),
+   checkAuth(...Object.keys(Role)),
   groupControllers.removeMember
 );
 
 // DELETE GROUP - Superadmin only
-router.delete('/:groupId', checkAuth(), groupControllers.deleteGroup);
+router.delete('/:groupId',  checkAuth(...Object.keys(Role)), groupControllers.deleteGroup);
 
 export const groupRouter = router;
