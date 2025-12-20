824f869d93de81bbfee091ceb79fd8082314e669
nayemalways <nishanahmed13913@gmail.com>
2025-12-20 15:06:27 +0600

    changed photon geocoding api by google geocoding api

--- src/app/utils/geocodeConvert.utils.ts
+++ src/app/utils/geocodeConvert.utils.ts
@@ -1,14 +1,15 @@
 import axios from "axios";
 import { ICoord } from "../modules/users/user.interface";
+import env from "../config/env";
 
 
 export const addressToLongLat = async (address: string): Promise<ICoord> => {
-  const photonUrl = `https://photon.komoot.io/api/?q=${encodeURIComponent(address.trim())}&limit=1`;
-  const response = await axios.get(photonUrl);
-  const coordinates = response.data.features[0].geometry.coordinates; // Convention: [long, lat]
+  const googleGeoCodingAPI = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${env.GOOGLE_MAP_API_KEY}`;
+  const response = await axios.get(googleGeoCodingAPI);
+  const coordinates = response.data.results[0].geometry.location 
 
   return {
-    long: coordinates[0],
-    lat: coordinates[1]
+    long: coordinates?.lng,
+    lat: coordinates?.lat
   };
 }
