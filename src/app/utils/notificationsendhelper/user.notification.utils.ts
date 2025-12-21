54406ec96d393b14a8495682ad1a65122aa8e901
Nayem Ahmed <nishanahmed13913@gmail.com>
2025-12-21 23:20:29 +0600

    configured personal notification by adding users notification preferences

--- src/app/utils/notificationsendhelper/user.notification.utils.ts
+++ src/app/utils/notificationsendhelper/user.notification.utils.ts
@@ -1,15 +1,22 @@
-import { Types } from "mongoose";
-import { INotification } from "../../modules/notifications/notification.interface";
-import { Notification } from "../../modules/notifications/notification.model";
-import { io } from "../../socket";
+import { Types } from 'mongoose';
+import { INotification } from '../../modules/notifications/notification.interface';
+import {
+  Notification,
+  NotificationPreference,
+} from '../../modules/notifications/notification.model';
+import { io } from '../../socket';
 
-
- 
-export const sendPersonalNotification = async ( payload: INotification ) => {
+export const sendPersonalNotification = async (payload: INotification) => {
   // Save to DB (for offline support)
   const notification = await Notification.create(payload);
 
-  const userRoom = (payload.user as Types.ObjectId).toString();
-  // Send real-time
-  io.to(userRoom).emit("notification", notification);
+  const receiverNotificationPreferences = await NotificationPreference.findById(
+    payload.user
+  );
+
+  if (receiverNotificationPreferences?.channel.inApp) {
+    const userRoom = (payload.user as Types.ObjectId).toString();
+    // Send real-time
+    io.to(userRoom).emit('notification', notification);
+  }
 };
