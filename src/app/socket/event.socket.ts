377245cb1a3cd94443fcdf5f78bed3b6e6c1d593
nayemalways <nishanahmed13913@gmail.com>
2025-12-22 16:27:00 +0600

    added 'join-group' event and created room

--- src/app/socket/event.socket.ts
+++ src/app/socket/event.socket.ts
@@ -1,15 +1,13 @@
 /* eslint-disable no-console */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export const SocketEvents = (socket: any) => {
-  // User joins event room
-  socket.on('join-event', (eventId: string) => {
-    socket.join(`event:${eventId.trim()}`);
-  });
 
   // User joins group room
   socket.on('join-group', (groupId: string) => {
-    socket.join(groupId.trim());
-    console.log(`User joined group room: ${groupId}`);
+    groupId.split(',').forEach((id: string) => {
+      socket.join(id.trim());
+      console.log(`User joined group room: ${id}`);
+    });
   });
 
   // User leaves group room
