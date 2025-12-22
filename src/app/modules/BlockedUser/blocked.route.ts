16bec902b37ad4f4cc6bd339dc44b29d5bdf4b2c
nayemalways <nishanahmed13913@gmail.com>
2025-12-22 14:13:27 +0600

    added comment inside the blocked.route.ts

--- src/app/modules/BlockedUser/blocked.route.ts
+++ src/app/modules/BlockedUser/blocked.route.ts
@@ -6,8 +6,11 @@ import { blockedUserController } from "./blocked.controller";
 
 const router = Router();
 
+// BLOCKED USER
 router.post("/", checkAuth(...Object.values(Role)), blockedUserController.blockedUser );
+// GET BLOCK LIST
 router.get('/', checkAuth(...Object.keys(Role)), blockedUserController.getBlockedUsers);
+// UNBLOCK USER
 router.get('/unblock/:blockedUserId', checkAuth(...Object.keys(Role)), blockedUserController.unblockUser);
 
 export const blockedUserRoutes = router;
