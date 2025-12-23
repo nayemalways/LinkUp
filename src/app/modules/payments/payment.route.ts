65b18451e565e6ceedbe41a51cf1eb89e9500f36
nayemalways <nishanahmed13913@gmail.com>
2025-12-23 18:05:19 +0600

    created Stripe payments api for direct-marchent bank transfer, Created Custom API to create Stripe connect account api for host or organizer, created checked api if the host or organizer has payout accoutn, created marchent payout bank account fetch api

---
+++ src/app/modules/payments/payment.route.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import { checkAuth } from "../../middlewares/auth.middleware";
+import { Role } from "../users/user.interface";
+import { paymentControllers } from "./payment.controller";
+
+const router = Router();
+
+// CREATE USER STRIPE CONNECT ACCOUNT
+router.get('/create_stripe_connect/:countryCode', checkAuth(...Object.keys(Role)), paymentControllers.createStripeConnectAccount);
+// CHECK IF USER HAS STRIPE CONNECT ACCOUNT
+router.get('/check_stripe_connect', checkAuth(...Object.keys(Role)), paymentControllers.checkAccountStatus);
+// GET CONNECTED PAYOUTS BANK ACCOUNT FROM STRIPE
+router.get('/connected_payout_bank_account', checkAuth(...Object.keys(Role)), paymentControllers.getConnectedBankAccount); 
+
+
+export const paymentRouter = router;
